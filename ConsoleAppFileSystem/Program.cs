e3a79a818b2bcdebbb43244efedfd6fd133d08f7
David Ikin <davidikin45@gmail.com>
2018-11-05 21:10:01 +0400

    Add project files.

---
+++ ConsoleAppFileSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppFileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
