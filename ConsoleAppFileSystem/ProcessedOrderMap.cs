0e97cd0bd97259737c2fbf422cb1a576314c2992
David Ikin <davidikin45@gmail.com>
2018-11-09 22:14:21 +0400

    commit

---
+++ ConsoleAppFileSystem/ProcessedOrderMap.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration;
+
+namespace ConsoleAppFileSystem
+{
+    class ProcessedOrderMap : ClassMap<ProcessedOrder>
+    {
+        public ProcessedOrderMap()
+        {
+            AutoMap();
+
+            Map(m => m.Customer).Name("CustomerNumber");
+            Map(m => m.Amount).Name("Quantity").TypeConverter<RomanTypeConverter>();
+        }
+    }
+}
--- ConsoleAppFileSystem/Program.cs
+++ ConsoleAppFileSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using static System.Console;
 
 namespace ConsoleAppFileSystem
 {
@@ -6,7 +7,59 @@ namespace ConsoleAppFileSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            WriteLine("Parsing command line options");
+
+            // Command line validation omitted for brevity
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                WriteLine("Invalid command line options");
+            }
+
+            WriteLine("Press enter to quit.");
+            ReadLine();
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            //var allFiles = Directory.GetFiles(directoryPath); // to get all files
+
+            switch (fileType)
+            {
+                case "TEXT":
+                    string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+                    foreach (var textFilePath in textFiles)
+                    {
+                        var fileProcessor = new FileProcessor(textFilePath);
+                        fileProcessor.Process();
+                    }
+                    break;
+                default:
+                    WriteLine($"ERROR: {fileType} is not supported");
+                    return;
+            }
         }
     }
 }
